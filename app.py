0bdbff2f194177c552f74321e18ae4bf28133e9d
thorory <298593716@qq.com>
2023-10-11 23:02:25 +0800

    init

--- app.py
+++ app.py
@@ -17,5 +17,21 @@ with open('config.json', "r", encoding="utf-8") as f:
     data = json.load(f)
 
 translator = Translator(api_key=data['api_key'], api_base=data['api_base'])
-resposne = translator.run("this is a test")
+translator.load_prompt_template()
+
+text = """
+This paper examines the causes and satisfaction consequences of work-to-family \ 
+and family-to-worksynergy for a sample of organizationally-employed parents while \ 
+controlling two types of work-familyconflict: work interfering with family and family \ 
+interfering with work. Participants included 1193respondents from the 2002 National \ 
+Study of the Changing Workforce who had a child under the age of 18years at home. \ 
+Work-family synergy is the frequency of experiencing positive energy and mood states \ 
+fromparticipating in both work and family. Synergy was significantly related to attitude \ 
+toward employerlearning opportunities, autonomy, job pressure, supervisor support, dependent \ 
+care, family income, mentalhealth, self-rated health, and satisfaction outcomes. Gender \ 
+similarities and differences in work-familysynergy were identified. Implications and directions \ 
+for future research are discussed.
+"""
+
+resposne = translator.run(input_language="English", output_language="Chinese", text=text)
 print(resposne)
--- models/paraphrase.py
+++ models/paraphrase.py
@@ -9,7 +9,7 @@ class Translator(Worker):
     def __init__(self, api_base, api_key, llm_model="gpt-3.5-turbo") -> None:
         super().__init__(api_base, api_key, llm_model)
 
-    def load_prompt_template(self, prompt_template_dir='./prompts/translator.txt'):
+    def load_prompt_template(self, prompt_template_dir='./prompts/paraphrase.txt'):
         if not os.path.exists(prompt_template_dir):
             raise FileNotFoundError("文件不存在")
         
@@ -18,13 +18,12 @@ class Translator(Worker):
 
         return self.prompt
     
-    def run(self, text="", input_language="English", output_language="Chinese"):
+    def run(self, text=""):
         input_template = "{text}"
         chat_prompt = ChatPromptTemplate.from_messages([
             ("system", self.prompt),
-            ("human", input_template),
         ])
-        messages = chat_prompt.format_messages(input_language=input_language, output_language=output_language, text=text)
         
+        messages = chat_prompt.format_messages(passage = text)
         print(messages)
         return super().run(messages)
